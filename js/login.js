e082b4fd7d0b7a5ecdbf3b29efd413caaf959ee6
zickz4g <lucianorosende@gmail.com>
2022-02-12 02:57:00 -0300

    new login js

--- js/login.js
+++ js/login.js
@@ -1,76 +1,79 @@
 $("#btnSend").click(() => {
 
-    let data = {
+    let data = [
+        $('#userID').val(),
+        $("#pwID").val(),
+        $("#email").val(),
+        $("#name").val(),
+        $("#surname").val()
+    ]
 
-        user: $('#userID').val(),
-        pw: $("#pwID").val(),
-        email: $("#email").val(),
-        name: $("#name").val(),
-        surname: $("#surname").val()
+    for(let i = 0; i < data.length; i++){
 
-    }
+        if (data[i] == ""){
+            Swal.fire({
+                icon: 'error',
+                text: 'Faltan datos para completar!'
+                
+              })
+            return false;
 
-    if (data.email == ""){
+        }
 
-        $("#resultado").html("<p class='errorDiv Bebas'>El Campo Email esta vacio!</p>").hide().fadeIn(800);
-        return false;
-        
-    }
-
-    else if (data.name == ""){
-
-        $("#resultado").html(`<p class='errorDiv Bebas'>El Campo NOMBRE esta vacio!</p>`).hide().fadeIn(800);
-        return false;
-    }
-
-    else if (data.surname == ""){
-
-        $("#resultado").html(`<p class='errorDiv Bebas'>El Campo APELLIDO esta vacio!</p>`).hide().fadeIn(800);
-        return false;   
-    }
-
-    else if (data.user == ""){
-        $("#resultado").html(`<p class='errorDiv Bebas'>El Campo USER esta vacio!</p>`).hide().fadeIn(800);
-        return false;
-
-    }
-
-    else if (data.pw == ""){
-        $("#resultado").html("<p class='errorDiv Bebas'>El Campo PASSWORD esta vacio!</p>").hide().fadeIn(800);
-        return false;
-    }
-
-    else{
-        $("#resultado").html("");
-    }
-
-    saveData(data.email, data.name, data.surname, data.user, data.pw);
-
-    $("#resultado").html("<p class='successDiv Bebas'>Los datos se guardaron correctamente!</p>").hide().fadeIn(800, () => {console.log("Estos son tus datos del Formulario")}).delay(2000).slideUp();
-
-
+    }        
+    saveData(data[0], data[1], data[2], data[3], data[4]);
     loadData();
-    
-    
+    loginScreen();
+
+   
 });
 
-function saveData (email, name, surname, userID, pw){
+function saveData (userID, pw, email, name, surname){
 
     let infoS = {
         Email: email,
         Nombre: name,
         Apellido: surname,
-        ID: userID,
-        password: pw
+        Id: userID,
+        Password: pw
     }
     localStorage.setItem("formulario_login", JSON.stringify(infoS));
 
 }
 
-function loadData(){
+ function loadData(){
 
 
-    let dataSaved = JSON.parse(localStorage.getItem("formulario_login"));
-    setTimeout(() => {console.log(dataSaved)}, 1500);
+   let dataSaved = JSON.parse(localStorage.getItem("formulario_login"));
+   console.log("Los datos del formulario");
+   console.log(dataSaved);
+   
     
+    
+    
+ }
+
+function loginScreen(){
+
+   let dataSaved = JSON.parse(localStorage.getItem("formulario_login"));
+
+    Swal.fire(
+        'Felicidades!',
+        'Completaste tus Datos!',
+        'success'
+      )
+
+    $(".log-change").html("");
+    $(".log-change").append(`<p class='successDiv Bebas'>Bienvenido <span class=''>${dataSaved.Id}!</span></p>`);
+    $(".reg-change").html("");
+    $(".reg-change").append(`<p class='successDiv Bebas'>YA ESTAS REGISTRADO!</p>`);
+    $(".sub-change").html("");
+    $(".sub-change").append(`<p class='successDiv Bebas'>TE SUSCRIBISTE!</p>`);
 }
+
+
+
+
+
+
+
